55cc11ea71f854f6d0ba416f67d1eca909e08878
Flandre-X <Flandre-X@protonmail.ch>
2018-05-05 03:15:19 -0500

    Add generic install.sh

--- install.sh
+++ install.sh
@@ -1,16 +1,7 @@
 #!/usr/bin/env bash
+# Run everything in scripts
+# cd to this directory before running this
 
-msg() { t=$1; shift; printf '%s: %s\n' "$t" "$@"; }
-
-_ln_home() {
-    [ -z "$2" ] && tgt="$HOME/$1" || tgt="$HOME/$2"
-    src="$PWD/$1"
-    [ -e "$tgt.bak" ] && rm "$tgt.bak"
-    [ -e "$tgt" ] && mv "$tgt" "$tgt.bak"
-    ln -sf "$src" "$tgt"
-}
-
-for d in config/*; do
-    msg INSTALL "$(basename "$d")"
-    _ln_home "$d" ".$d"
+for f in install.d/*.sh; do
+    [ -e "$f" ] && "$f"
 done
