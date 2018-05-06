7e902334a94de009ca141487512d513c2404c362
Flandre-X <Flandre-X@protonmail.ch>
2018-05-06 10:52:32 -0500

    Add zshrc and linking to install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,15 @@
 # Run everything in scripts
 # cd to this directory before running this
 
+src_dir="$(pwd)"
+dest_dir="$HOME"
+for f in "$(cat to-install)"; do
+    src="$src_dir/$f"
+    dest="$dest_dir/.$f"
+    [ -e "$dest" ] && cp "$dest" "$dest.bak"
+    ln -sf "$src" "$dest"
+done
+
 for f in install.d/*.sh; do
     [ -e "$f" ] && "$f"
 done
