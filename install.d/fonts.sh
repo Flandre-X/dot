a1acc5bdeb569565472621d8c62c703b34cee566
Flandre-X <Flandre-X@protonmail.ch>
2018-05-17 12:51:12 -0500

    Add San Francisco font installation

--- install.d/fonts.sh
+++ install.d/fonts.sh
@@ -39,14 +39,36 @@ nerd_fonts ()
     )
 }
 
+san_francisco_dir="$font_dir/san_francisco"
+san_francisco ()
+{
+    local url='https://github.com/AppleDesignResources/SanFranciscoFont'
+
+    mkdir -p "$san_francisco_dir"
+    git clone --depth 1 "$url" "$san_francisco_dir"
+    sf_fonts=("$san_francisco_dir"/*.otf)
+    if [ ${#sf_fonts[@]} -ne 0 ]; then
+	cp -t "$font_dir" "${sf_fonts[@]}"
+    else
+	return 1
+    fi
+}
+
 if [ ! -e "$ibm_plex_dir" ]; then
     # A hack that causes the function to exit upon the first command that
     # returns non-zero while not causing the whole program to exit
     # (set -e required in function body)
+    echo 'INSTALLING IBM Plex™ Nerd Font'
     set +e
     (set -e; ibm_plex)
 fi
+if [ ! -e "$san_francisco_dir" ]; then
+    echo 'INSTALLING San Francisco font'
+    set +e
+    (set -e; san_francisco)
+fi
 if [ ! -e "$nerd_fonts_dir" ]; then
+    echo 'INSTALLING Nerd Fonts (this may take a while)'
     set +e
     (set -e; nerd_fonts)
 fi
